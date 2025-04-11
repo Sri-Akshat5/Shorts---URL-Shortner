540406f25c5f2a33a8d838f497d5aba499c94f3e
Akshat Srivastava <akshatsrivastava566@gmail.com>
2025-04-11 23:50:04 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -18,12 +18,10 @@ app.use("/api/links", linkRoutes);
 app.get("/:shortCode", handleRedirect);
 
 
-mongoose.connect(process.env.MONGO_URI,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected"))
-.catch((err)=> console.error(err));
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error("MongoDB connection error:", err));
+  
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`server running on port ${PORT}`));
